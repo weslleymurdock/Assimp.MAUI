775d9e950aaaaffd3b7967d90391901f5e817178
Weslley Murdock <weslleyluiz@outlook.com.br>
2026-09-23 20:00:00 -0300

    feat: add initial project and package reference

---
+++ sample/Assimp.MAUI.Sample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Assimp.MAUI.Sample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
